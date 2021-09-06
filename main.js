257dfbf085146009ed41fb36d10741689f3f2710
himani412 <77782996+himani412@users.noreply.github.com>
2021-09-06 05:42:22 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 video="";
 status="";
+objects="";
+
 function preload(){
     video= createVideo('video.mp4');
     video.hide();
@@ -10,6 +12,29 @@ function setup(){
 }
 function draw(){
    image(video,0,0,480,380);
+   if(status != ""){
+       objectDetector.detect(video, gotresult);
+       for(i = 0; i < objects.length ; i++){
+           document.getElementById("status").innerHTML= "Status : Objects Detected";
+           document.getElementById("numberofobjects").innerHTML="Number of Objects Detected are :  "+objects.length;
+           fill("#FF0000");
+           percent= floor(objects[i].confidence*100);
+           text(objects[i].label+ " " + percent + "%" , objects[i].x+15 , objects[i].y+15);
+           nofill();
+           stroke("#FF0000");
+           rect(objects[i].x , objects[i].y, objects[i].width , objects[i].height);
+
+       }
+   }
+}
+function gotresult(error,results){
+    if(error){
+        console.log(error);
+
+    }
+  
+        console.log(results);
+objects= results;
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
